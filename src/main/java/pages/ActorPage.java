bc2e8b2161bae28cacd2c7a04e7271d5d214d8b1
sromankov <Sergey.Romankov@dataart.com>
2018-08-03 16:22:41 +0300

    added findAll

--- src/main/java/pages/ActorPage.java
+++ src/main/java/pages/ActorPage.java
@@ -1,13 +1,19 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
 public class ActorPage extends BasePage {
 
-    @FindBy(xpath = "(.//h2/span[@id='Filmography' or @id='Selected_filmography']/../following-sibling::ul)[1]/li/i")
+
+    @FindAll({
+            @FindBy(xpath = ".//h3/span[@id='Filmography' or @id='Selected_filmography']/../following-sibling::div/ul/li"),
+            @FindBy(xpath = "(.//h2/span[@id='Filmography' or @id='Selected_filmography']/../following-sibling::ul)[1]/li/i"),
+            @FindBy(xpath = "(.//*[@id='Filmography']/../following-sibling::table)[1]/tbody/tr/td[2]//a")
+    })
     private List<WebElement> movies;
 
     public boolean FilmographyContainsMovie(String expectedMovie) {
